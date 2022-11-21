c7ed34974303d41afd4e54b5d5d1408d24ad24ac
ALAN JOSE <alanjose04231120@gmail.com>
2022-11-20 23:41:14 -0500

    created 2

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,10 +1,10 @@
-const path = '../db.json';
+const path = require('../db/db.json');
 const router = require('express').Router();
 const fs = require('fs');
 
 
 router.get('/notes',  (req, res) => {
- let data = fs.readFileSync(path);
+ let data = fs.readFileSync('./db/db.json');
 let data2 = JSON.parse(data);
 return data2;
 
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const api2 = require('./Develop/routes/apiroutes');
+const api2 = require('./routes/apiroutes');
 const path = require('path');
 const PORT = 5000;
-const api = require('./noteroutes');
+const api = require('./routes/noteroutes');
 
 
 const app = express();
@@ -10,13 +10,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('Develop/public'));
 app.use('/',api)
-app.use('/api',api2)
+// app.use('/api',api2)
 
 
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '../mod11chall/public/index.html'));
 });
 
 app.listen(PORT, () =>
